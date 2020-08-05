b07531d9037a3d25d7298a836d161efbdd9c0fca
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-05 09:53:21 -0300

    Add a new fieldset to TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -25,6 +25,7 @@ const TeacherForm = () => {
           <Input name="whatsapp" label="Whatsapp" />
           <Textarea name="bio" label="Biografia" />
         </fieldset>
+
         <fieldset>
           <legend>Sobre a aula</legend>
           <Select
@@ -43,6 +44,31 @@ const TeacherForm = () => {
           />
           <Input name="cost" label="Custo da sua hora por aula" />
         </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+          <div className="schedule-item">
+            <Select
+              name="week-day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-feira" },
+                { value: "2 Física", label: "Terça-feira" },
+                { value: "3", label: "Quarta-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sábado" },
+              ]}
+            />
+            <Input name="from" label="Das" type="time"/>
+            <Input name="to" label="Até" type="time"/>
+          </div>
+        </fieldset>
+
         <footer>
           <p>
             <img src={warningIcon} alt="Aviso importante" />

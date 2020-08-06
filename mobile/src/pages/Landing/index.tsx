4e8f054316fb6b2667bee2d0adbe9a79d2ba6698
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-06 14:18:22 -0300

    Create React Native Folder Structure + Create Landing page with a few features

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import landingImg from "../../assets/images/landing.png";
+
+import styles from "./styles";
+
+const Landing = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+      <Text style={styles.title}>
+        Seja bem-vindo, {"\n"}
+        <Text style={styles.titleBold}>
+          O que deseja fazer?
+        </Text>
+      </Text>
+    </View>
+  );
+}
+
+export default Landing;

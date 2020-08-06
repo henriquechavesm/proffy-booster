bc82ac840cbaf3b2aed35eb648526eb7c8c1b3e1
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-06 16:25:02 -0300

    Create PageHeader

--- mobile/src/pages/Favourites/index.tsx
+++ mobile/src/pages/Favourites/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-import styles from "./styles"
+import PageHeader from "../../components/PageHeader";
+
+import styles from "./styles";
 
 const Favourites = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Favourites</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus Proffys favoritos" />
+    </View>
+  );
+};
 
-export default Favourites
+export default Favourites;

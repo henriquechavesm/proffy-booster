e754b1a22736796fb1a363681ad6b80c8fac65ac
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-09 19:23:06 -0300

    Add TeacherItem and ScrollView to TeacherList and Favourites pages

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ScrollView } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
@@ -10,10 +10,15 @@ const TeacherList = () => {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16}}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 };

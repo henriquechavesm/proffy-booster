af0fc9a1637bca0a17241d3ceb152c6064e06fdf
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-09 19:18:12 -0300

    Create TeacherItem component

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
-import styles from "./styles"
+import styles from "./styles";
 
 const TeacherList = () => {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader title="Proffys disponíveis" />
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
     </View>
   );
 };

73602de474b412bc931a0629234816b3631d1b76
Henrique Chaves <henriquechavesmm@gmail.com>
2020-08-09 20:01:34 -0300

    Add filters to TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Text, TextInput } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
@@ -9,10 +9,27 @@ import styles from "./styles";
 const TeacherList = () => {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput style={styles.input} placeholder="Qual a matéria?" />
+          
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput style={styles.input} placeholder="Qual o dia?"/>
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput style={styles.input} placeholder="Qual o horário?"/>
+            </View>
+          </View>
+        </View>
+      </PageHeader>
       <ScrollView
         style={styles.teacherList}
-        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16}}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
         <TeacherItem />
         <TeacherItem />
